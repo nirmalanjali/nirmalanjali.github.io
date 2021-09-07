ec25b63f5ab4ce1218d8b1beb6a59f76098db6a7
nirmalanjali <89961243+nirmalanjali@users.noreply.github.com>
2021-09-07 11:37:21 +0530

    Update form.js

--- form.js
+++ form.js
@@ -8,6 +8,41 @@ var message = "";
         		data: {message: message},
         		dataType: "json"
   		});
-    		alert('Thanks for the email, we\'ll be in touch promptly.');
     		return false;
 		});
+
+function pushNotify() {
+  new Notify({
+    status: 'success',
+    title: 'Notify Title',
+    text: 'Notify text lorem ipsum',
+    effect: 'fade',
+    speed: 300,
+    customClass: null,
+    customIcon: null,
+    showIcon: true,
+    showCloseButton: true,
+    autoclose: false,
+    autotimeout: 3000,
+    gap: 20,
+    distance: 20,
+    type: 1,
+    position: 'right top'
+  })
+}
+
+let myNotify
+
+function pushNotify() {
+  myNotify = new Notify({
+    status: 'success',
+    title: 'Notify Title',
+    text: 'notify text',
+    effect: 'slide',
+    type: 3
+  })
+}
+
+function close() {
+  myNotify.close()
+}
